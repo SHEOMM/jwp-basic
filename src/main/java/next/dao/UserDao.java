ea3b5d35c212d93a646ffb86f5302c076ccea98d
SHEOMM <esh0821@snu.ac.kr>
2023-12-30 19:32:23 +0900

    chapter 7.1 회원 데이터를 DB에 저장하기

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -25,23 +25,48 @@ public class UserDao {
 
             pstmt.executeUpdate();
         } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
+            closeConnection(con, pstmt);
         }
     }
 
     public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try{
+            con = ConnectionManager.getConnection();
+            String sql = "UPDATE USERS SET userId=?, password=?, name=?, email=? WHERE userId=?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, user.getUserId());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getEmail());
+            // 말이 안되는 로직인데
+            pstmt.setString(5, user.getUserId());
+            pstmt.executeUpdate();
+        }finally {
+            closeConnection(con, pstmt);
+        }
     }
 
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try{
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT userId, password, name, email FROM USERS";
+            pstmt = con.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            List<User> users = new ArrayList<>();
+            while(rs.next()){
+                users.add(new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                        rs.getString("email")));
+            }
+            return users;
+        }finally {
+            closeConnection(con, pstmt, rs);
+        }
+
     }
 
     public User findByUserId(String userId) throws SQLException {
@@ -64,15 +89,29 @@ public class UserDao {
 
             return user;
         } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
+            closeConnection(con, pstmt, rs);
+        }
+    }
+
+    private void closeConnection(Connection con, PreparedStatement pstmt, ResultSet rs) throws SQLException {
+        if (rs != null) {
+            rs.close();
+        }
+        if (pstmt != null) {
+            pstmt.close();
+        }
+        if (con != null) {
+            con.close();
+        }
+    }
+
+    private void closeConnection(Connection con, PreparedStatement pstmt) throws SQLException {
+
+        if (pstmt != null) {
+            pstmt.close();
+        }
+        if (con != null) {
+            con.close();
         }
     }
 }

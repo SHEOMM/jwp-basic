6d8da9046e66ae429afff1ac300819f2d8fbc67f
SHEOMM <esh0821@snu.ac.kr>
2024-01-12 00:05:15 +0900

    기본 리플렉션 테스트 과제 구현

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,20 @@
 package core.ref;
 
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Method;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            if(declaredMethod.isAnnotationPresent(MyTest.class)){
+                assertEquals(declaredMethod.getName().length(), 3);
+            }
+        }
     }
 }
--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,5 +1,13 @@
 package core.ref;
 
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Optional;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,18 +21,41 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+        assertEquals(clazz.getName(), "next.model.Question");
+        logger.debug("constructor" +Arrays.toString(clazz.getDeclaredConstructors()));
+        logger.debug("fields" + Arrays.toString(clazz.getDeclaredFields()));
+        logger.debug("method "+ Arrays.toString(clazz.getDeclaredMethods()));
     }
     
     @Test
-    public void newInstanceWithConstructorArgs() {
+    public void newInstanceWithConstructorArgs()
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<User> clazz = User.class;
-        logger.debug(clazz.getName());
+        Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+        Constructor<?> declaredConstructor1 = declaredConstructors[0];
+        User user = (User)declaredConstructor1.newInstance("userId", "password", "name", "email");
+        assertEquals(user.getUserId(), "userId");
+        assertEquals(user.getPassword(), "password");
+        assertEquals(user.getName(), "name");
+        assertEquals(user.getEmail(), "email");
+
     }
     
     @Test
-    public void privateFieldAccess() {
+    public void privateFieldAccess() throws IllegalAccessException {
         Class<Student> clazz = Student.class;
-        logger.debug(clazz.getName());
+        Student student = new Student();
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            declaredField.setAccessible(true);
+            if(declaredField.getName().equals("name")){
+                declaredField.set(student, "hi");
+            }else{
+                declaredField.set(student, 12);
+            }
+
+        }
+        assertEquals(student.getName(), "hi");
+        assertEquals(student.getAge(), 12);
     }
 }
